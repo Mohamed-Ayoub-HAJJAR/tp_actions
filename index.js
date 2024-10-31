6584233de8782fef6eb961431b48dd98e68e7e63
mohamed ayoub hajjar <mohamed.ayoub.hajjar@gmail.com>
2024-10-31 15:33:37 +0100

    okok

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const port = 3000;
 app.use(express.static('views'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port from ci/cd ${port}`)
 });
